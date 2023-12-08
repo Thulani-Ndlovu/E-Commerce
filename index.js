8efcd26541d7a9c85969644d76f442f3d1c1bc95
Thulani Ndlovu <thulani.ndlovu.54922@gmail.com>
2023-12-08 14:22:15 +0200

    Server Updates

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import bodyParser from "body-parser";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import session from 'express-session';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const session = require('express-session');
+
 const app = express();
 const port = 3000;
-
-var express = require("express");
-var router = express.Router();
+const router = express.Router();
 
 const credentials = {
   email: "adminUser1@gmail.com",
@@ -21,6 +20,21 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Set the views directory
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")); //step 2
+
+
+// step 1: rendering the homepage
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
 // Sign In Page
 app.get('/', (req, res) => {
@@ -59,18 +73,6 @@ router.get('/logout', (req, res)=> {
   })
 })
 
-// step 1: rendering the homepage
-app.get("/", (req, res) => {
-  res.render("home");
-})
-
-app.get("/contact", (req, res) => {
-    res.render("contact");
-})
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
 
 
-module.exports = router;
+//module.exports = router;
