03b94b0f48fcd56c771e8c016c7b2d7029d5a74a
Thami911 <thamimncwabe@gmail.com>
2023-12-22 21:14:59 +0200

    clean code with description by comments

--- userInfo_DB.js
+++ userInfo_DB.js
@@ -1,25 +1,39 @@
+// Import the 'mysql2/promise' library for MySQL operations
 import mysql from 'mysql2/promise';
+
+// Load environment variables from a .env file
 import 'dotenv/config';
 
+// Create a MySQL connection pool using the provided configuration
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST,               // Database host
+  user: process.env.DB_USER,               // Database user
+  database: process.env.DB_NAME,           // Database name
+  password: process.env.DB_PASSWORD,       // Database password
+  waitForConnections: true,               // Whether the pool should wait for connections
+  connectionLimit: 10,                    // Maximum number of connections in the pool
+  queueLimit: 0                           // Maximum number of connection requests the pool will queue
 });
 
+// Function to establish a connection to the database and return the connection
 async function connectToDatabase() {
   try {
+    // Attempt to get a connection from the pool
     const connection = await pool.getConnection();
+
+    // Log a message if the connection is successful
     console.log('Connected to the database');
+
+    // Return the obtained connection
     return connection;
   } catch (error) {
+    // Log an error message if connection fails
     console.error('Error connecting to the database:', error);
-    process.exit(1); // Exit the process if connection fails
+
+    // Exit the process if connection fails
+    process.exit(1);
   }
 }
 
+// Export the MySQL connection pool and the connectToDatabase function for external use
 export { pool, connectToDatabase };
